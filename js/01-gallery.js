5abb56d20f8556b7b4a172de90b65e6e5960ca99
DenisChornui <129185799+DenisChornui@users.noreply.github.com>
2023-08-13 17:11:09 +0300

    comtaska2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,12 +1,43 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const container = document.querySelector(".gallery");
 
+function createMarkup(arr) {
+  return arr
+    .map(
+      ({ preview, original, description }) => `
+    <li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img 
+    class="gallery__image" 
+    src="${preview}" 
+    alt="${description}"
+    data-source="${original}">
+    </a>
+  </li>`
+    )
+    .join("");
+}
+container.insertAdjacentHTML("beforeend", createMarkup(galleryItems));
 
-// Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї.
-// Реалізація делегування на ul.gallery і отримання url великого зображення.
-// Підключення скрипту і стилів бібліотеки модального вікна basicLightbox. Використовуй CDN сервіс jsdelivr і додай у проект посилання на мініфіковані (.min) файли бібліотеки.
-// Відкриття модального вікна по кліку на елементі галереї. Для цього ознайомся з документацією і прикладами.
-// Заміна значення атрибута src елемента <img> в модальному вікні перед відкриттям. Використовуй готову розмітку модального вікна із зображенням з прикладів бібліотеки basicLightbox.
+container.addEventListener("click", handlerProductClick);
 
+function handlerProductClick(evt) {
+  evt.preventDefault();
+  if (evt.target === evt.currentTarget) {
+    return;
+  }
+
+  const instance = basicLightbox.create(`
+	<img  
+    src="${evt.target.dataset.source}" 
+    alt="${evt.target.alt}">
+`);
+  instance.show();
+
+  container.addEventListener("keydown", (evt) => {
+    if (evt.code === "Escape") {
+      instance.close();
+    }
+  });
+}
